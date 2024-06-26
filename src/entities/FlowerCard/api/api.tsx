4cb8ac37472eb55713450f71576465eb4c94cb6f
Ozod <o.bukhorizoda@mail.ru>
2024-06-26 16:13:55 +0500

    feat: add swr for reguests

--- src/entities/FlowerCard/api/api.tsx
+++ src/entities/FlowerCard/api/api.tsx
@@ -1,9 +1,19 @@
-export const FlowerCardApi = async () => {
-  try {
-    const res = await fetch("http://localhost:5000/plants");
-    const data = await res?.json();
-    return data;
-  } catch (e) {
-    console.log(e);
-  }
+// export const FlowerCardApi = async () => {
+//   try {
+//     const res = await fetch("http://localhost:5000/plants");
+//     const data = await res?.json();
+//     return data;
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
+
+import useSWR from "swr";
+const fetcher = (url: string) => fetch(url).then((r) => r.json());
+export const useFlowerCard = () => {
+  const { data, error, isLoading } = useSWR(
+    "http://localhost:5000/plants",
+    fetcher
+  );
+  return { data, error, isLoading };
 };
--- src/pages/home/ui/FlowerCards/FlowerCards.tsx
+++ src/pages/home/ui/FlowerCards/FlowerCards.tsx
@@ -1,8 +1,13 @@
+"use client";
 import { FlowerCardApi } from "@/src/entities/FlowerCard";
+import { useFlowerCard } from "@/src/entities/FlowerCard/api/api";
 import { Flower } from "@/src/entities/FlowerCard/lib/types";
 
-const FlowerCards = async () => {
-  const data = await FlowerCardApi();
+const FlowerCards = () => {
+  // const data = await FlowerCardApi();
+  const { data, error, isLoading } = useFlowerCard();
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error ocurred</div>;
   return (
     <div>
       {data?.map((flower: Flower) => {
